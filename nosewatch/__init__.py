fdbb91b4efc314d54cc4f559f40b3ffe2d48d1a6
Cezar Berea <berea.cezar@gmail.com>
2014-01-29 21:44:13 +0200

    Wire up pyinotify to watch some files

--- nosewatch/__init__.py
+++ nosewatch/__init__.py
@@ -1,23 +1,60 @@
 import sys
+import os
 import nose
+import pyinotify
+import logging
 
 class Logger(object):
 
     def info(self, message):
         print message
 
-
 class TestRunner(object):
     def run(self):
-        nose.main()
+        logging.info("Starting tests.")
+        nose.main(exit=False)
+
+class FileTester(object):
+    def test(self, filename):
+        logging.debug("Start testing %s", filename)
+        return True
 
 
-from pyinotify import WatchManager, Notifier, EventsCodes, ProcessEvent
+class FileEventHandler(pyinotify.ProcessEvent):
+    def __init__(self, runner, tester):
+        self.runner = runner
+        self.tester = tester
+
+    def process_default(self, event):
+        logging.debug("Event caught %s - %s", event.pathname, event.maskname)
+        self.process_file(event.pathname)
+
+    def process_file(self, filename):
+        if self.tester.test(filename):
+            logging.info("File %s changed", filename)
+            self.runner.run()
 
 def run():
-    logger = Logger()
-    runner = TestRunner()
-    logger.info('Starting watcher with args: %s' % sys.argv[1:])
-    runner.run()
+    if os.environ.get('DEBUG', False):
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
-# watcher
+    logging.info('Starting watcher with args: %s' % sys.argv[1:])
+
+    MASK = pyinotify.IN_DELETE  \
+         | pyinotify.IN_CREATE \
+         | pyinotify.IN_MODIFY
+
+    path = os.getcwd()
+    logging.debug("Current working dir: %s", path)
+
+    runner = TestRunner()
+    tester = FileTester()
+
+    watch_manager = pyinotify.WatchManager()
+    handler = FileEventHandler(runner, tester)
+    notifier = pyinotify.Notifier(watch_manager, handler)
+    watch_descriptor = watch_manager.add_watch(path, MASK, rec=True)
+
+    notifier.loop()
